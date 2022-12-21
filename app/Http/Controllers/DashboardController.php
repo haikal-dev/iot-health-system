d1b85a1d1f193f6f31f7ef623320e1e6954f267c
Haikal Azizan <mhaikalazizan@gmail.com>
2022-12-21 23:40:33 +0800

    Create one dummy bot for telegram message

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\TelegramController as TG;
 
 class DashboardController extends Controller
 {
     public function index(Request $request){
+        // TG::message('dashboard is up!')->send();
         return view('dashboard.index');
     }
 }
--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -6,8 +6,13 @@ use App\Http\Controllers\TelegramAPIController;
 
 class TelegramController
 {
-    protected static $chatId = '495215238';
-    protected static $robotId = '5983822956:AAHVJxpwdfQdy-Vx-8WrGen-y_J4-fu0k5A';
+    // need to adjust this property for development
+    // protected static $chatId = '495215238';
+    // protected static $robotId = '5983822956:AAHVJxpwdfQdy-Vx-8WrGen-y_J4-fu0k5A';
+
+    // development bot
+    protected static $chatId = '495913083';
+    protected static $robotId = '1856574595:AAF4sAkhwscWJOimZAKIbhDyC4dlgOJKCYA';
 
     public static function message($msg){
         $bot = new TelegramAPIController;
