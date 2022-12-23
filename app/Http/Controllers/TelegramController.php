88f059f397589c0fa550cd69c6b7ce8bcc726928
Haikal Azizan <mhaikalazizan@gmail.com>
2022-12-23 10:21:43 +0800

    Change bot

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -6,13 +6,12 @@ use App\Http\Controllers\TelegramAPIController;
 
 class TelegramController
 {
-    // need to adjust this property for development
-    // protected static $chatId = '495215238';
-    // protected static $robotId = '5983822956:AAHVJxpwdfQdy-Vx-8WrGen-y_J4-fu0k5A';
+    protected static $chatId = '495215238';
+    protected static $robotId = '5983822956:AAHVJxpwdfQdy-Vx-8WrGen-y_J4-fu0k5A';
 
     // development bot
-    protected static $chatId = '495913083';
-    protected static $robotId = '1856574595:AAF4sAkhwscWJOimZAKIbhDyC4dlgOJKCYA';
+    // protected static $chatId = '495913083';
+    // protected static $robotId = '1856574595:AAF4sAkhwscWJOimZAKIbhDyC4dlgOJKCYA';
 
     public static function message($msg){
         $bot = new TelegramAPIController;
