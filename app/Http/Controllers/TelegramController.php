7caaae9670d4a7cd6fef84e2291ffd9b025e7bef
Haikal Azizan <mhaikalazizan@gmail.com>
2022-12-23 10:35:08 +0800

    Get telegram data from app .env

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -6,17 +6,10 @@ use App\Http\Controllers\TelegramAPIController;
 
 class TelegramController
 {
-    protected static $chatId = '495215238';
-    protected static $robotId = '5983822956:AAHVJxpwdfQdy-Vx-8WrGen-y_J4-fu0k5A';
-
-    // development bot
-    // protected static $chatId = '495913083';
-    // protected static $robotId = '1856574595:AAF4sAkhwscWJOimZAKIbhDyC4dlgOJKCYA';
-
     public static function message($msg){
         $bot = new TelegramAPIController;
-        $bot->setChatId(self::$chatId);
-        $bot->setRobot(self::$robotId);
+        $bot->setChatId(env('TELEGRAM_ID'));
+        $bot->setRobot(env('TELEGRAM_BOT'));
         $bot->text($msg);
         
         return $bot;
